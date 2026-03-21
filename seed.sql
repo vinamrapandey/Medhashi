626fe81d4c15e4b206eadde42bd0892b299f8e35
vinamrapandey <vinamrapandey22@gmail.com>
2026-03-21 05:30:26 +0530

    Update seed and config for Medhashi deployment

--- seed.sql
+++ seed.sql
@@ -1,8 +1,2 @@
-INSERT INTO workspaces (id, couple_names, wedding_date, plan, admin_pin_hash)
-VALUES (
-  'medhashi-aands-2026',
-  'Apoorva & Saumya',
-  '2026-04-27',
-  'baraat',
-  '2026'
-);
+INSERT OR IGNORE INTO workspaces (id, couple_names, wedding_date, plan, admin_pin_hash)
+VALUES ('medhashi-aands-2026', 'Apoorva & Saumya', '2026-04-27', 'baraat', 'medhashi2026');
